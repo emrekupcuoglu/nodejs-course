3d5cd4e29a3e9ba79844284330d783e494312ba5
ecerenarslan <114579376+ecerenarslan@users.noreply.github.com>
2022-09-29 00:44:58 +0300

    Merging changes from Ceren's computer (#2)
    * third commit
    
    * Fourth commit

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -4,22 +4,50 @@
 // of the different callbacks queues all at the same time.
 // That situation happens when a lot of request are
 // coming into your app, but it is really hard to reolicate locally.
-
+const http = require("http");
 const fs = require("fs");
 const crypto = require("crypto");
 
+// For timing the amount of time that the processes take
 const start = Date.now();
+
+// !Changing the default thread pool size
+// unfortunately we can't change the threal pool with the code below
+// UV_THREADPOOL_SIZE=1
+// on Windows. It works on macOS and linux
+// To change it on windows we need to change it on the command line before running the script
+// We can do it like this
+// set UV_THREADPOOL_SIZE=1 & node event-loop.js
+// or make a script with these instructions, add it to the package.json
+// and run it using the npm
+// "scripts": {
+//   "start": "set UV_THREADPOOL_SIZE=1 & node event-loop.js"
+// },
 
 // * First the top-level code is executed
 // * But the order of the other 3 operations
-// *are not determined by the event loop
+// * are not determined by the event loop
 // * Because they are not running in the event loop just yet.
 // For that we need to move the setTimeout and the setImmediate inside a callback function
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
+let a = 0;
+for (let i = 0; i < 10000000000; i++) {
+  a++;
+}
+console.log(a);
+
+console.log(Date.now() - start, "for loop 1 finished");
 
 fs.readFile("./test-file.txt", "utf-8", () => {
-  console.log("I/O finished");
+  console.log(Date.now() - start, "file read finished");
+  let a = 0;
+  for (let i = 0; i < 10000000000; i++) {
+    a++;
+  }
+  console.log(a);
+  console.log(Date.now() - start, "for loop 2 finished");
+
   console.log("---------- Running in the event loop --------------");
 
   // *We have put the setTimeout and the setImmediate inside this callback
@@ -40,10 +68,14 @@ fs.readFile("./test-file.txt", "utf-8", () => {
   process.nextTick(() => console.log("process.nextTick"));
 
   // *Thread Pool
-  // By default the size of the thread pool is 4 
+  // By default the size of the thread pool is 4
   // There are 4 threads doing computation at the same time
   // Thats why these 4 took aproximately the same time
   // and happened basically at the same time.
+
+  // *The sync version blocks the event loop
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password encrypted");
 
   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
     console.log(Date.now() - start, "password encrypted");
@@ -57,6 +89,13 @@ fs.readFile("./test-file.txt", "utf-8", () => {
   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
     console.log(Date.now() - start, "password encrypted");
   });
+
+  let b = 0;
+  for (let i = 0; i < 10000000000; i++) {
+    b++;
+  }
+  console.log(b);
+  console.log(Date.now() - start, "for loop 3 finished");
 });
 
 console.log("Hello from the top-level code");
@@ -72,3 +111,62 @@ console.log("Hello from the top-level code");
 // be executed right away after the pollin phase even befeor the expired timer.
 // In this example the timer expires right away but the event loop pauses
 // in the polling phase so that the setImmediate callback is executed first.
+
+// !!!!!!!!!!!!IMPORTANT!!!!!!!!!!
+// If the callback of an async function has synchronous code it will execute as synchronous.
+// If there is an asynch function after the sync code it will have to
+// wait for the sync code to finish. To avoid this put the async function above the synch code,
+// this way async code still needs to waits the synch code to finish to output it's response
+// but it will be running in the background as the sync code runs.
+// So instead of starting to execute after the sync code finishes,
+// it will start before the sync code
+// but will wait the sync code to finish to return the result of it's callback function
+
+// So when the callback function is put in the execution stack it acts like a top level code
+// All the code in it will be run synchronously, if there is an async code in it,
+// event loop will put it in the thread pool and when it finishes it's operation
+// it will call the callback function of the async code to the
+// execution stack to be run if the execution stack is empty;
+// if it isn't empty it will wait until the execution stacks empty then put it in the execution stack.
+
+// ?Event Driven Architecture
+// The modules we used lşke the http,fs,crypto, timer are buşlt around an event driven architecture.
+// We can use this architecture to our own advanteage
+// In node therea re certain objects called event emitters that emit events
+// as soon as somethin important happes in the app like a request hitting a server
+// timer expiring or a files finishing to read.
+// These events then can be picked up by event listeners that we developers set up
+// which will fire callback functions that are attached to each listener.
+//  let' take a look at how node uses the event driven architecture to handle
+// server requests.
+const server = http.createServer();
+server.on("request", (req, res) => {
+  console.log("Request recieved");
+  res.end("Reques received");
+});
+// This implemantation is a bit different than the one we did before
+// but it works the exact same way.
+// server.on method is actually how we create a listener
+// In this case a listener for the reques event
+// When we have our server running and a new request is made
+// server acts as an emitter. Server will emit an event each time
+// a request hits the server.
+// Since we already have a listener set up for this event
+// the callback function we attached to this listener will be called.
+// It works this way because behind the scenes server is
+// actually an instance of the nodejs EventEmitter class.
+// So it inherits all this event emitting and listening logic from
+// the EventEmitter class
+// *This event emitter logic is called the observer patter in the JavaScript programming.
+// The idea is that there is an observer which is the event listener in this case
+// which keeps observing the subject that will eventally emit the
+// event that the listener is waiting for.
+// The opposite of this pattern is simply functions calling other functions.
+// The observer patterns was designed to react rather than to call.
+// The huge benefit of using this patters is that everything is de-copuled.
+// For example we don't have functions from the files ystem module calling
+// functions from the http module because that would be a huge mess
+// Instead these modules are nicely de-coupled and self contained, each emitting events
+// other funcitons -even if they come from other modules- can respond to.
+// Also using an event driven architecture makes it way more straightforward to
+//react multiple times to the same event. ALl we have to do is to set up multiple listeners.
---
+++ 2-how-node-works/starter/events.js
@@ -0,0 +1,64 @@
+// ?Events in Practice
+
+// To use the built in node events we nee to require the events module
+// EventEmitter is a class
+const EventEmitter = require("events");
+const http = require("http");
+
+// !If you were to use this pattern in real life ist is best to create a new clas that inherits from the node event emitter
+// This mechanism of extending the EventEmitter class is basically how other core node modules
+// like https, fs and many others implement events internally.
+// So all of them inherit from the EventEmitter class
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+// To create a new emitter we create an instance of the class we imported
+const myEMitter = new Sales();
+// EventEmitters can emit named events and we can listen to these event (basically subscribe to them)
+// and react accordingly.
+// It's a bit like setting up an event listener on a DOM event.
+
+// We are creating the listener and
+// the callback function that will be executed as soon as the event is emitted.
+myEMitter.on("newSale", () => {
+  console.log("There was a new sale");
+});
+
+// Let's add another event listener, one of the nice things anout event emitters
+// is that we can add multile listeners for the same event.
+myEMitter.on("newSale", () => {
+  console.log("Customer name: Emre");
+});
+
+myEMitter.on("newSale", (stock) => {
+  console.log(`There are ${stock} items remaining in stock`);
+});
+
+// Let's pretend that we are building an online store
+// We can make up any event name we want.
+// emitter emits the event and the listeners observe the emitter
+// and wait until it emits a new sale event.
+// We can also pass in arguments to the event listener by passing them as
+// additional arguments in the emitter
+myEMitter.emit("newSale", 9);
+
+//##################
+// *Creating a small web server and listening to events it emits
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request recieved");
+  res.end("Request recieved");
+});
+server.on("request", (req, res) => {
+  console.log("Another request recieved");
+});
+
+server.on("close", () => console.log("Server closed"));
+
+// We start the server with server.listen
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
