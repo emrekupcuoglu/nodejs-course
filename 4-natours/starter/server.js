c204d21dc17954161678940a7227d3eca8515ca5
emrekupcuoglu <86340151+emrekupcuoglu@users.noreply.github.com>
2022-10-17 00:05:13 +0300

    Files about mongoose added

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,14 +1,30 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-// To use the variables inside the config.env file
+// To use the variables inside of the config.env file
 // we need to call the dotenv.config() with the path to the config.env file
 // This command will read our variables from the file
 // and save them into node.js environment variables
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+// First argument is the connection string
+// mongoose returns a promise
+mongoose
+  .connect(DB)
+  // This promise gets acces to a connection object
+  .then(con => {
+    // console.log(con.connections);
+    console.log("DB connection successful");
+  });
 
 // To see which environment we are in
-// This is set by epxress
+// This is set by express
 console.log(app.get("env"));
 
 // node.js also sets a lot of environment variables
